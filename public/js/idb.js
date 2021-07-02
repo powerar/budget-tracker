4be6845d88743861f83bda616f0611a54fd8cdb3
powerar <arpower@bell.net>
2021-07-02 14:36:08 -0400

    added indexeddb

---
+++ public/js/idb.js
@@ -0,0 +1,35 @@
+//variable to hold db connection
+let db;
+
+//create connected to IndexedDB
+const request = indexedDB.open('budget_tracker', 1);
+
+request.onupgradeneeded = function(event) {
+    const db = event.target.result;
+
+    db.createObjectStore('new_budget',  { autoIncrement: true });
+};
+
+request.onsuccess = function(event) {
+    db = event.target.result;
+
+    if (navigator.onLine) {
+
+    }
+};
+
+request.onerror = function(event) {
+    console.log(event.target.errorCode);
+};
+
+function saveRecord(record) {
+    const transaction = db.transaction(['new_budget'], 'readwrite');
+
+    const budgetObjectStore = transaction.objectStore('new_budget');
+
+    budgetObjectStore.add(record);
+};
+
+function uploadedBudget() {
+    
+}
